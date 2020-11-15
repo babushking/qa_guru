a3f1f530a7e7e8240ea3eb0a8b0c45471ceb2e05
Anton Babushking <napishi_antonu@mail.ru>
2020-11-15 20:41:32 +0300

    test register form

---
+++ src/test/java/GoogleTests.java
@@ -0,0 +1,21 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GoogleTests {
+
+    @Test
+    void selenideSearchTest() {
+        // Открыть google
+        open("https://google.com");
+
+        // Ввести Selenide в поиск
+        $(byName("q")).setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(Condition.text("selenide.org"));
+    }
+}
